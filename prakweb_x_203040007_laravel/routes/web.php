1dff3f96077a4b5246bc57f169b286aad9ce97ef
Rifaldi0007 <rifaldia513@gmail.com>
2022-10-17 22:09:29 +0700

    Menyelesaikan video 4

--- prakweb_x_203040007_laravel/routes/web.php
+++ prakweb_x_203040007_laravel/routes/web.php
@@ -14,18 +14,84 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',[
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "name" => "Faldi arsandi",
+       "title" => "About",
+       "name" => "Faldi arsandi",
         "email" => "rifaldia513@gmail.com",
         "image" => "Faldi.jpg"
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view('posts');
+    
+$blog_posts = [
+    [
+        "title" => "judul post pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Falz",
+        "body" => "Lorem ipsum dolor sit amet, consectetur 
+        adipisicing elit. Nesciunt explicabo ipsa expedita, alias aliquid molestias voluptas laboriosam quam neque. Possimus quam veritatis similique sapiente suscipit repudiandae voluptatum, rem commodi
+        exercitationem corrupti necessitatibus illo debitis, nesciunt accusantium. Ea molestias praesentium fugiat quas libero quam suscipit placeat eum incidunt provident est quaerat, reiciendis doloribus esse omnis quasi soluta excepturi autem deleniti? Vero ad sit, corporis earum impedit voluptatum provident 
+        quibusdam, eligendi optio assumenda minus a temporibus commodi dolores magni expedita praesentium rem."
+    ],
+
+    [
+        "title" => "judul post kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Dodi",
+        "body" => "Lorem ipsum dolor sit amet, consectetur 
+        adipisicing elit. Nesciunt explicabo ipsa expedita, alias aliquid molestias voluptas laboriosam quam neque. 
+        Possimus quam veritatis similique sapiente suscipit repudiandae voluptatum, rem commodi"
+    ],
+];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
 });
 
+
+//halaman single post
+Route ::get('posts/{slug}', function($slug) {
+
+    $blog_posts = [
+        [
+            "title" => "judul post pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Falz",
+            "body" => "Lorem ipsum dolor sit amet, consectetur 
+            adipisicing elit. Nesciunt explicabo ipsa expedita, alias aliquid molestias voluptas laboriosam quam neque. Possimus quam veritatis similique sapiente suscipit repudiandae voluptatum, rem commodi
+            exercitationem corrupti necessitatibus illo debitis, nesciunt accusantium. Ea molestias praesentium fugiat quas libero quam suscipit placeat eum incidunt provident est quaerat, reiciendis doloribus esse omnis quasi soluta excepturi autem deleniti? Vero ad sit, corporis earum impedit voluptatum provident 
+            quibusdam, eligendi optio assumenda minus a temporibus commodi dolores magni expedita praesentium rem."
+        ],
+    
+        [
+            "title" => "judul post kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Dodi",
+            "body" => "Lorem ipsum dolor sit amet, consectetur 
+            adipisicing elit. Nesciunt explicabo ipsa expedita, alias aliquid molestias voluptas laboriosam quam neque. 
+            Possimus quam veritatis similique sapiente suscipit repudiandae voluptatum, rem commodi"
+        ],
+    ];
+
+    $new_post =[];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "single post",
+        "post" => $new_post
+    ]);
+});
