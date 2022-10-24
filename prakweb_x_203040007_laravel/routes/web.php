c5146e31a25e918a6170d488e7cb811faa452138
Rifaldi0007 <rifaldia513@gmail.com>
2022-10-24 08:44:53 +0700

    Menyelesaikan video ke 5 lanjutan Laravel

--- prakweb_x_203040007_laravel/routes/web.php
+++ prakweb_x_203040007_laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,69 +32,7 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    
-$blog_posts = [
-    [
-        "title" => "judul post pertama",
-        "slug" => "judul-post-pertama",
-        "author" => "Falz",
-        "body" => "Lorem ipsum dolor sit amet, consectetur 
-        adipisicing elit. Nesciunt explicabo ipsa expedita, alias aliquid molestias voluptas laboriosam quam neque. Possimus quam veritatis similique sapiente suscipit repudiandae voluptatum, rem commodi
-        exercitationem corrupti necessitatibus illo debitis, nesciunt accusantium. Ea molestias praesentium fugiat quas libero quam suscipit placeat eum incidunt provident est quaerat, reiciendis doloribus esse omnis quasi soluta excepturi autem deleniti? Vero ad sit, corporis earum impedit voluptatum provident 
-        quibusdam, eligendi optio assumenda minus a temporibus commodi dolores magni expedita praesentium rem."
-    ],
-
-    [
-        "title" => "judul post kedua",
-        "slug" => "judul-post-kedua",
-        "author" => "Dodi",
-        "body" => "Lorem ipsum dolor sit amet, consectetur 
-        adipisicing elit. Nesciunt explicabo ipsa expedita, alias aliquid molestias voluptas laboriosam quam neque. 
-        Possimus quam veritatis similique sapiente suscipit repudiandae voluptatum, rem commodi"
-    ],
-];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
+Route::get('/blog', [PostController::class, 'index']); 
 
 //halaman single post
-Route ::get('posts/{slug}', function($slug) {
-
-    $blog_posts = [
-        [
-            "title" => "judul post pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Falz",
-            "body" => "Lorem ipsum dolor sit amet, consectetur 
-            adipisicing elit. Nesciunt explicabo ipsa expedita, alias aliquid molestias voluptas laboriosam quam neque. Possimus quam veritatis similique sapiente suscipit repudiandae voluptatum, rem commodi
-            exercitationem corrupti necessitatibus illo debitis, nesciunt accusantium. Ea molestias praesentium fugiat quas libero quam suscipit placeat eum incidunt provident est quaerat, reiciendis doloribus esse omnis quasi soluta excepturi autem deleniti? Vero ad sit, corporis earum impedit voluptatum provident 
-            quibusdam, eligendi optio assumenda minus a temporibus commodi dolores magni expedita praesentium rem."
-        ],
-    
-        [
-            "title" => "judul post kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Dodi",
-            "body" => "Lorem ipsum dolor sit amet, consectetur 
-            adipisicing elit. Nesciunt explicabo ipsa expedita, alias aliquid molestias voluptas laboriosam quam neque. 
-            Possimus quam veritatis similique sapiente suscipit repudiandae voluptatum, rem commodi"
-        ],
-    ];
-
-    $new_post =[];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "single post",
-        "post" => $new_post
-    ]);
-});
+Route ::get('posts/{slug}', [PostController::class, 'show']);
