29415b008e9ba26d8869f06ccf0ac16b56550fa5
Rifaldi0007 <rifaldia513@gmial.com>
2022-10-30 23:45:45 -0700

    menyelesaikan video laravel ke 9

--- prakweb_x_203040007_laravel/database/seeders/DatabaseSeeder.php
+++ prakweb_x_203040007_laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +18,48 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Falz',
+            'email' => 'faldiarsan524@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Indra',
+            'email' => 'indragta@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name'=> 'Programming',
+            'slug'=> 'Programming'
+        ]);
+
+        Category::create([
+            'name'=> 'Personal',
+            'slug'=> 'Personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'Judul_Pertama',
+            'excerpt' => 'Programming adalah kegiatan membuat set kode instruksi untuk program (sofware) yang dijalankan oleh komputer. ',
+            'body' => ' Programming adalah kegiatan membuat set kode instruksi untuk program (sofware) yang dijalankan oleh komputer. Agar program tersebut bisa berjalan dengan baik, programming juga melibatkan proses menulis, menguji, memperbaiki, dan melakukan pengecekan ulang pada kode instruksi yang sudah dibuat.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul kedua',
+            'slug' => 'Judul_kedua',
+            'excerpt' => 'Nama saya Indra gta saya lahir dibandung. ',
+            'body' => ' Nama saya Indra gta saya lahir dibandung tgl 15 may 2000 saya menjadi mahasiswa itb dan sudah menginjak tingkat 3.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- prakweb_x_203040007_laravel/resources/views/posts.blade.php
+++ prakweb_x_203040007_laravel/resources/views/posts.blade.php
@@ -3,15 +3,18 @@
 @extends('layouts.main')
 
 @section('container')
-    
+    <h1 class="mb-5">Halaman Blog Post</h1>
+
     @foreach($posts as $post)
-        <article class="mb-5">
-            <h2>
-                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-            </h2>
-            <h5>By: {{ $post["author"] }}</h5>
-            {{ $post->excerpt }}
-            </article>
+        <article class="mb-5 border-bottom pb-4">
+            <h2><a href="/posts\{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
+
+            <p>By. <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
+            <p>{{ $post->excerpt }}</p>
+
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More..</a>
+        </article>
     @endforeach
 
 @endsection
