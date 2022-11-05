1e99e18b14cc2a14366a089a5e35654bacc008e1
Rifaldi0007 <rifaldia513@gmail.com>
2022-11-05 09:56:47 +0700

    menyelesaikan video 14 View Login dan Registration

--- prakweb_x_203040007_laravel/routes/web.php
+++ prakweb_x_203040007_laravel/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
-Use App\Models\Category;
-Use App\Models\User;
 
 
 /*
@@ -66,3 +66,7 @@ Route::get('/categories', function() {
 //        'posts' => $author->posts->load('category', 'author')
 //    ]);
 //});
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
