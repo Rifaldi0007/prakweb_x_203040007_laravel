528f156d2f8638c149cec4f7530f2b89c273cd83
Rifaldi0007 <rifaldia513@gmail.com>
2022-11-27 18:00:19 +0700

    Menyelesaikan Video User Login & Middleware

--- prakweb_x_203040007_laravel/resources/views/partials/navbar.blade.php
+++ prakweb_x_203040007_laravel/resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container">
-      <a class="navbar-brand" href="/">Falz Blog</a>
+      <a class="navbar-brand" href="/">Fix Blog</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -21,10 +21,27 @@
         </ul>
 
         <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-                <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i>Sign In</a>
-            </li>
-        </ul>
+            @auth
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Welcome back, {{ auth()->user()->name }}
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i>My Dashboard</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <form action="/logout" method="post">
+                  @csrf
+                  <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i>Logout</button>
+              </form>
+          </li>
+      </ul>
+  </li>
+            @else
+          <li calss="nav-item">
+              <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+          </li>
+          @endauth
+      </ul>
 
       </div>
     </div>
