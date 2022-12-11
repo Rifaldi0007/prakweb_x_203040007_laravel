2f0789fd842befeff55ead0fb7792ee8aae21ba8
Rifaldi0007 <rifaldia513@gmail.com>
2022-12-11 19:48:26 +0700

    menyelesaikan Video 23

--- prakweb_x_203040007_laravel/routes/web.php
+++ prakweb_x_203040007_laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
@@ -81,3 +82,5 @@ Route::get('/dashboard', function() {
 Route::get('dashboard/posts/checkSlug', [DashboardController::class, 'checkSlug'])
 ->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
